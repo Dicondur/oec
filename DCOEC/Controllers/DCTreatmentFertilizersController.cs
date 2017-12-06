37aed1b7eee8fc1546ed3aceca91141e678445ca
Dinesh <dinesh.condur@gmail.com>
2017-12-06 17:00:22 -0500

    Draft

---
+++ DCOEC/Controllers/DCTreatmentFertilizersController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DCOEC.Models;
+
+namespace DCOEC.Controllers
+{
+    public class DCTreatmentFertilizersController : Controller
+    {
+        private readonly OECContext _context;
+
+        public DCTreatmentFertilizersController(OECContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DCTreatmentFertilizers
+        public async Task<IActionResult> Index()
+        {
+            var oECContext = _context.TreatmentFertilizer.Include(t => t.FertilizerNameNavigation).Include(t => t.Treatment);
+            return View(await oECContext.ToListAsync());
+        }
+
+        // GET: DCTreatmentFertilizers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatmentFertilizer = await _context.TreatmentFertilizer
+                .Include(t => t.FertilizerNameNavigation)
+                .Include(t => t.Treatment)
+                .SingleOrDefaultAsync(m => m.TreatmentFertilizerId == id);
+            if (treatmentFertilizer == null)
+            {
+                return NotFound();
+            }
+
+            return View(treatmentFertilizer);
+        }
+
+        // GET: DCTreatmentFertilizers/Create
+        public IActionResult Create()
+        {
+            ViewData["FertilizerName"] = new SelectList(_context.Fertilizer, "FertilizerName", "FertilizerName");
+            ViewData["TreatmentId"] = new SelectList(_context.Treatment, "TreatmentId", "TreatmentId");
+            return View();
+        }
+
+        // POST: DCTreatmentFertilizers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TreatmentFertilizerId,TreatmentId,FertilizerName,RatePerAcre,RateMetric")] TreatmentFertilizer treatmentFertilizer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(treatmentFertilizer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FertilizerName"] = new SelectList(_context.Fertilizer, "FertilizerName", "FertilizerName", treatmentFertilizer.FertilizerName);
+            ViewData["TreatmentId"] = new SelectList(_context.Treatment, "TreatmentId", "TreatmentId", treatmentFertilizer.TreatmentId);
+            return View(treatmentFertilizer);
+        }
+
+        // GET: DCTreatmentFertilizers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatmentFertilizer = await _context.TreatmentFertilizer.SingleOrDefaultAsync(m => m.TreatmentFertilizerId == id);
+            if (treatmentFertilizer == null)
+            {
+                return NotFound();
+            }
+            ViewData["FertilizerName"] = new SelectList(_context.Fertilizer, "FertilizerName", "FertilizerName", treatmentFertilizer.FertilizerName);
+            ViewData["TreatmentId"] = new SelectList(_context.Treatment, "TreatmentId", "TreatmentId", treatmentFertilizer.TreatmentId);
+            return View(treatmentFertilizer);
+        }
+
+        // POST: DCTreatmentFertilizers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TreatmentFertilizerId,TreatmentId,FertilizerName,RatePerAcre,RateMetric")] TreatmentFertilizer treatmentFertilizer)
+        {
+            if (id != treatmentFertilizer.TreatmentFertilizerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(treatmentFertilizer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TreatmentFertilizerExists(treatmentFertilizer.TreatmentFertilizerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FertilizerName"] = new SelectList(_context.Fertilizer, "FertilizerName", "FertilizerName", treatmentFertilizer.FertilizerName);
+            ViewData["TreatmentId"] = new SelectList(_context.Treatment, "TreatmentId", "TreatmentId", treatmentFertilizer.TreatmentId);
+            return View(treatmentFertilizer);
+        }
+
+        // GET: DCTreatmentFertilizers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatmentFertilizer = await _context.TreatmentFertilizer
+                .Include(t => t.FertilizerNameNavigation)
+                .Include(t => t.Treatment)
+                .SingleOrDefaultAsync(m => m.TreatmentFertilizerId == id);
+            if (treatmentFertilizer == null)
+            {
+                return NotFound();
+            }
+
+            return View(treatmentFertilizer);
+        }
+
+        // POST: DCTreatmentFertilizers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var treatmentFertilizer = await _context.TreatmentFertilizer.SingleOrDefaultAsync(m => m.TreatmentFertilizerId == id);
+            _context.TreatmentFertilizer.Remove(treatmentFertilizer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TreatmentFertilizerExists(int id)
+        {
+            return _context.TreatmentFertilizer.Any(e => e.TreatmentFertilizerId == id);
+        }
+    }
+}
---
+++ DCOEC/Controllers/DCTreatmentsController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DCOEC.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace DCOEC.Controllers
+{
+    public class DCTreatmentsController : Controller
+    {
+        private readonly OECContext _context;
+
+        public DCTreatmentsController(OECContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DCTreatments
+        public async Task<IActionResult> Index(int? plotId, string farmName)
+        {
+
+            ViewBag.name = "";
+
+            //store to session
+            if (plotId != null)
+            {
+
+
+                //Set Session
+
+                // create session variables
+                HttpContext.Session.SetInt32("_plotId", Convert.ToInt32(plotId));
+                //HttpContext.Session.SetString("_farmName", farmName);
+                TempData["Message"] = "Plot ID stored to session.";
+
+            }
+            else //(plotId== null)
+            {
+                if (HttpContext.Session.GetString("_plotId") != null)
+                {
+                    plotId = Convert.ToInt32(HttpContext.Session.GetInt32("_plotId"));
+                    //farmName = HttpContext.Session.GetString("_farmName");
+
+
+                    //Set View Data
+                    ViewData["PlotID"] = plotId;
+                    ViewData["farmName"] = farmName;
+
+                    TempData["Message"] = "Got Plot ID from session.";
+
+                }
+                else
+                {
+
+                    TempData["Message"] = "Select a Plot first";
+                    return RedirectToAction("Index", "DCPlots");
+                }
+
+
+            }
+
+
+
+            foreach(var x in _context.TreatmentFertilizer) {
+                ViewBag.name = x.FertilizerNameNavigation.FertilizerName;
+                //if (x.FertilizerNameNavigation.FertilizerName is null)
+                //{
+
+                //    ViewBag.name = "No fertilizer";
+                //}
+                //else
+                //{
+                //    ViewBag.name = x.FertilizerNameNavigation.FertilizerName + "+";
+                //}
+
+            }
+        
+
+
+
+            var oECContext = _context.Treatment
+                                    .Include(t => t.Plot)
+                                    .Where (x => x.PlotId == plotId);
+            return View(await oECContext.ToListAsync());
+        }
+
+        // GET: DCTreatments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatment = await _context.Treatment
+                .Include(t => t.Plot)
+                .SingleOrDefaultAsync(m => m.TreatmentId == id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            return View(treatment);
+        }
+
+        // GET: DCTreatments/Create
+        public IActionResult Create()
+        {
+            ViewData["PlotId"] = new SelectList(_context.Plot, "PlotId", "PlotId");
+            return View();
+        }
+
+        // POST: DCTreatments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TreatmentId,Name,PlotId,Moisture,Yield,Weight")] Treatment treatment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(treatment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlotId"] = new SelectList(_context.Plot, "PlotId", "PlotId", treatment.PlotId);
+            return View(treatment);
+        }
+
+        // GET: DCTreatments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatment = await _context.Treatment.SingleOrDefaultAsync(m => m.TreatmentId == id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+            ViewData["PlotId"] = new SelectList(_context.Plot, "PlotId", "PlotId", treatment.PlotId);
+            return View(treatment);
+        }
+
+        // POST: DCTreatments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TreatmentId,Name,PlotId,Moisture,Yield,Weight")] Treatment treatment)
+        {
+            if (id != treatment.TreatmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(treatment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TreatmentExists(treatment.TreatmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlotId"] = new SelectList(_context.Plot, "PlotId", "PlotId", treatment.PlotId);
+            return View(treatment);
+        }
+
+        // GET: DCTreatments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treatment = await _context.Treatment
+                .Include(t => t.Plot)
+                .SingleOrDefaultAsync(m => m.TreatmentId == id);
+            if (treatment == null)
+            {
+                return NotFound();
+            }
+
+            return View(treatment);
+        }
+
+        // POST: DCTreatments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var treatment = await _context.Treatment.SingleOrDefaultAsync(m => m.TreatmentId == id);
+            _context.Treatment.Remove(treatment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TreatmentExists(int id)
+        {
+            return _context.Treatment.Any(e => e.TreatmentId == id);
+        }
+    }
+}
